bd539bbf88e162981b73c50d57f30c9da665b68a
Devvyl <dev.artyom@mail.ru>
2025-11-09 14:39:07 +0700

    fix: src/lexer.cpp:122:22: предупреждение: многознаковая символьная константа [-Wmultichar]

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -3,114 +3,75 @@
 #include <iostream>
 
 namespace lexer {
+
+    void flush_buffer(std::string& buf, int& mod, std::vector<token>& tokens) {
+         if (mod == 1) {
+             if (buf == "PRINT" || buf == "VARINT" || buf == "VARSTR" ||
+                 buf == "TYPE" || buf == "INPUT" || buf == "SUM" || buf == "SUB") {
+                 tokens.push_back({"KWORD", buf});
+             } else {
+                 tokens.push_back({"WORD", buf});
+             }
+         } else if (mod == 2) {
+             tokens.push_back({"DIGIT", buf});
+         }
+         buf = "";
+         mod = 0;
+     }
+
     std::vector<token> lexering(std::string code) {
         std::vector<token> tokens;
-        std::string buffer;
-        int mod = 0;
-        /*
-        0 - буфер пустой
-        1 - буквы
-        2 - цифры
-        */
         std::string buf = "";
+        int mod = 0;
         token newtoken;
-        for (int i = 0; i != code.size(); i++) {
+
+        for (int i = 0; i < code.size(); i++) {
             char symb = code[i];
-            if (!(std::isalpha(symb)) && mod == 1) {
-                if (buf == "PRINT"
-                ||  buf == "VARINT"
-                ||  buf == "VARSTR"
-                ||  buf == "TYPE"
-                ||  buf == "INPUT"
-                ||  buf == "SUM"
-                ||  buf == "SUB"
-                ) {
+
+            if (!std::isalpha(symb) && mod == 1) {
+                if (buf == "PRINT" || buf == "VARINT" || buf == "VARSTR" ||
+                    buf == "TYPE" || buf == "INPUT" || buf == "SUM" || buf == "SUB") {
                     newtoken = {"KWORD", buf};
-                    tokens.push_back(newtoken);
-                    buf = "";
-                    mod = 0;
-                }
-                else {
+                } else {
                     newtoken = {"WORD", buf};
-                    tokens.push_back(newtoken);
-                    buf = "";
-                    mod = 0;
                 }
+                tokens.push_back(newtoken);
+                buf = "";
+                mod = 0;
             }
-            else if (!(std::isalpha(symb)) && mod == 2) {
+            else if (!std::isdigit(symb) && mod == 2) {
                 newtoken = {"DIGIT", buf};
                 tokens.push_back(newtoken);
                 buf = "";
                 mod = 0;
             }
+
             switch (symb) {
                 case '=':
-                    if (code[i+1] == '=') {
+                    if (i + 1 < code.size() && code[i+1] == '=') {
                         i++;
-                        newtoken = {"EQUALS", "NONE"};
-                        tokens.push_back(newtoken);
-                    }
-                    else {
-                        newtoken = {"EQUAL", "NONE"};
-                        tokens.push_back(newtoken);
+                        tokens.push_back({"EQUALS", "NONE"});
+                    } else {
+                        tokens.push_back({"EQUAL", "NONE"});
                     }
                     break;
-                case ' ':
-                    newtoken = {"SPACE", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
-                case '+':
-                    newtoken = {"PLUS", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
-                case '-':
-                    newtoken = {"MINUS", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
-                case '*':
-                    newtoken = {"STAR", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
-                case '%':
-                    newtoken = {"MOD", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
-                case '/':
-                    newtoken = {"SLASH", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
-                case '(':
-                    newtoken = {"L_PAREN", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
-                case ')':
-                    newtoken = {"R_PAREN", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
+                case ' ': tokens.push_back({"SPACE", "NONE"}); break;
+                case '+': tokens.push_back({"PLUS", "NONE"}); break;
+                case '-': tokens.push_back({"MINUS", "NONE"}); break;
+                case '*': tokens.push_back({"STAR", "NONE"}); break;
+                case '%': tokens.push_back({"MOD", "NONE"}); break;
+                case '/': tokens.push_back({"SLASH", "NONE"}); break;
+                case '(': tokens.push_back({"L_PAREN", "NONE"}); break;
+                case ')': tokens.push_back({"R_PAREN", "NONE"}); break;
                 case '\'':
-                case '\"':
-                    newtoken = {"QUOTE", "NONE"};
-                    tokens.push_back(newtoken);
-                    break;
+                case '\"': tokens.push_back({"QUOTE", "NONE"}); break;
                 case '\\':
-                    if (code[i+1] == 'n') {
+                    if (i + 1 < code.size()) {
+                        if (code[i+1] == 'n') { tokens.push_back({"SPECSYMBOL", "\n"}); }
+                        else if (code[i+1] == 't') { tokens.push_back({"SPECSYMBOL", "\t"}); }
+                        else if (code[i+1] == 'v') { tokens.push_back({"SPECSYMBOL", "\v"}); }
+                        else { std::cout << "\nLEXERFAIL: Unknown symbol"; exit(10); }
                         i++;
-                        newtoken = {"SPECSYMBOL", "\n"};
-                        tokens.push_back(newtoken);
-                    }
-                    else if (code[i+1] == 't') {
-                        i++;
-                        newtoken = {"SPECSYMBOL", "\t"};
-                        tokens.push_back(newtoken);
-                    }
-                    else if (code[i+1] == 'v') {
-                        i++;
-                        newtoken = {"SPECSYMBOL", "\v"};
-                        tokens.push_back(newtoken);
-                    }
-                    else {
-                        std::cout<<std::endl<<"LEXERFAIL: Uwnkown symbol";
-                        exit(10);
                     }
                     break;
                 case '.':
@@ -119,85 +80,63 @@ namespace lexer {
                 case '!':
                 case '@':
                 case '#':
-                case '№':
                 case '$':
                 case ';':
                 case '&':
                 case '_':
                     buf += symb;
-                    newtoken = {"SYMBOL", buf};
-                    tokens.push_back(newtoken);
+                    tokens.push_back({"SYMBOL", buf});
                     buf = "";
                     break;
+                case '\xD0':
+                    if (i + 1 < code.size() && code[i+1] == '\x9E') {
+                        buf += "№";
+                        tokens.push_back({"SYMBOL", buf});
+                        buf = "";
+                        i++;
+                        break;
+                    }
+                    [[fallthrough]];
                 default:
                     if (std::isalpha(symb)) {
-                        if (buf == "") {
+                        if (buf.empty()) {
                             buf += symb;
                             mod = 1;
-                        }
-                        else {
+                        } else {
                             if (mod == 1) {
                                 buf += symb;
-                            }
-                            else {
-                                if (buf == "PRINT"
-                                ||  buf == "VARINT"
-                                ||  buf == "VARSTR"
-                                ||  buf == "TYPE"
-                                ||  buf == "INPUT"
-                                ||  buf == "SUM"
-                                ||  buf == "SUB"
-                                ) {
-                                    newtoken = {"KWORD", buf};
-                                    tokens.push_back(newtoken);
-                                    buf = "";
-                                    mod = 0;
-                                }
-                                else {
-                                    newtoken = {"WORD", buf};
-                                    tokens.push_back(newtoken);
-                                    buf = "";
-                                    mod = 0;
-                                }
+                            } else {
+                                flush_buffer(buf, mod, tokens);
                             }
                         }
-                        break;
                     }
-                    else if (std::isdigit(symb)){
-                        if (buf == "") {
+                    else if (std::isdigit(symb)) {
+                        if (buf.empty()) {
                             buf += symb;
                             mod = 2;
-                        }
-                        else {
+                        } else {
                             if (mod == 2) {
                                 buf += symb;
-                            }
-                            else {
-                                newtoken = {"DIGIT", buf};
-                                tokens.push_back(newtoken);
-                                buf = "";
-                                mod = 0;
+                            } else {
+                                flush_buffer(buf, mod, tokens);
                             }
                         }
-                        break;
                     }
                     else {
-                        buffer = "";
-                        buffer += symb;
-                        newtoken = {"SYMBOL", buffer};
-                        tokens.push_back(newtoken);
-                        break;
+                        tokens.push_back({"SYMBOL", std::string(1, symb)});
                     }
+                    break;
             }
         }
-        if (buf != "") {
+
+        if (!buf.empty()) {
             if (mod == 1) {
-                newtoken = {"WORD", buf};
+                tokens.push_back({"WORD", buf});
             } else if (mod == 2) {
-                newtoken = {"DIGIT", buf};
+                tokens.push_back({"DIGIT", buf});
             }
-            tokens.push_back(newtoken);
         }
+
         return tokens;
     }
 }
